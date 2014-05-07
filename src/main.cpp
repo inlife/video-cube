af7da586088ebac9bf4f2b4a7b36e3d8ee57c1b4
Denis Sergienko <olol.toor@gmail.com>
2014-05-07 21:40:50 +0300

    basic cgi app

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,8 @@
-/* main.cpp */
+#include <iostream>
+#include <cstdlib>
+int main(){
+  std::cout<<"Content-type: text/html\r\n\r\n";
+
+  std::cout<<"QUERY STRING IS:  ";
+  std::cout<<std::getenv("QUERY_STRING"); // get enviroment variable
+}
