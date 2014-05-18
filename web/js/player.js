e3b55673df8a01861b0b9a6e806ef5e196c65874
Denis Sergienko <olol.toor@gmail.com>
2014-05-18 21:41:22 +0300

    comments

--- web/js/player.js
+++ web/js/player.js
@@ -11,37 +11,41 @@
 //     var player = new Player('video', '/users/1/video/2/', 40, watch_function);
 //     player.startVideo();
 
+// elementId - id дива где будет видео
+// video_path - папка видео
+// duration - количество кусков
+// watch - функция, которая вызывается когда просмотрено больше половины
 
 function Player(elementId , video_path, duration, watch){
   this.videoPath  = video_path;
   this.highPath   = this.videoPath+'720/';
-  this.mediumPath = this.videoPath+'360/';
+  this.mediumPath = this.videoPath+'360/'; // папки с разными качествами
   this.poorPath   = this.videoPath+'240/';
   this.globalId   = elementId;
   this.wrapperId  = 'video_player_wrapper';
-  this.counter = 0;
-  this.partCounter = 10;
-  this.playable = true;
-  this.inner = '';
-  this.parts = parseInt(duration);
-  this.quality = 1;
-  this.lock = true;
-  this.newTime = false;
-  this.fullscreen = false;
-  this.watch = watch;
-  this.watched = false;
-  this.createDom();
-  this.volume = 0.7;
+  this.counter = 0; // текущий кусок видео
+  this.partCounter = 10; // сколько секунд текущего куска просмотрено
+  this.playable = true; // не пауза
+  this.parts = parseInt(duration); // общее количество кусков
+  this.quality = 1; // среднее качество
+  this.lock = true; // false когда происходит загрузка нового куска
+  this.newTime = false; // произошла ли перемотка
+  this.fullscreen = false; // на полный ли мы экран
+  this.watch = watch; // функция набивания просмотров
+  this.watched = false;  // вызывали ли мы функцию набивания просмотров
+  this.createDom(); // вызываем функцию наполнения дива
+  this.volume = 0.7; // громкость 70%
 
   $(this).on("loaded", { player: this }, function(event){
-    event.data.player.createVideo(event.data.player.newVideoId);
+    event.data.player.createVideo(event.data.player.newVideoId);  // вызывается когда нужно создать новый див с видео
   })
 }
 Player.prototype.createVideo = function(itemId){
   if(document.getElementById(itemId)){ // если уже такой открывок есть
     return false;
   }
-  videoItem = $("<video id='"+itemId+"' src='"+(this.getPath()+this.counter)+".mp4?"+Math.random()+"'></video>");
+  videoItem = $("<video id='"+itemId+"' src='"+(this.getPath()+this.counter)+".mp4?"+Math.random()+"'></video>"); // создали <video>
+  // рандом от кеширования
 
   this.loadStarted();
   videoItem.on('loadedmetadata', { player: this }, function(event){
@@ -66,10 +70,11 @@ Player.prototype.showVideo = function(itemId){
   item.style.height  = '100%';
   this.partCounter = 0;
 }
-Player.prototype.update = function() {
+Player.prototype.update = function() { // вызывается каждую секунду
+  // вывод отладочной информации
   console.log({lock: this.lock, part: this.counter, playable: this.playable, counter: this.partCounter, quality: this.quality, fullscreen: this.fullscreen});
 
-  if(this.counter > (this.parts/2)){
+  if(this.counter > (this.parts/2)){ // если просмотрено, то вызываем функцию набивания просмотров
     if(!this.watched){
       this.watch();
       this.watched = true;
@@ -78,37 +83,37 @@ Player.prototype.update = function() {
 
   if((this.counter<this.parts)&&(this.partCounter>=10)&&this.playable&&this.lock){
 
-    if(this.counter == 0 || this.newTime) {
+    if(this.counter == 0 || this.newTime) { // только начали смотреть или только что перемотали
       this.newTime = false;
 
-      newVideoId = this.globalId+this.counter;
+      newVideoId = this.globalId+this.counter; // id video
 
-      this.createVideo(newVideoId);
-      this.showVideo(newVideoId);
+      this.createVideo(newVideoId); // создаем <video>
+      this.showVideo(newVideoId); // и сразу играем
 
-      this.currentVideoId = newVideoId;
+      this.currentVideoId = newVideoId; // для работы громкости
 
       this.counter++;
       this.newVideoId = this.globalId+this.counter;
-
-      // setTimeout(function(){ this.createVideo(this.newVideoId) }.bind(this), 1000);
 
       this.partCounter++;
 
     } else {
 
       this.oldVideoId = this.globalId+(this.counter-1);
-      this.hideVideo(this.oldVideoId);
+      this.hideVideo(this.oldVideoId); // прячем кусок который только что воспроизводился
 
       this.currentVideoId = this.globalId+this.counter;
-      this.showVideo(this.currentVideoId);
+      this.showVideo(this.currentVideoId); // показываем новый кусок
 
       this.counter++;
-      this.newVideoId = this.globalId+this.counter;
+      this.newVideoId = this.globalId+this.counter; // выставляем айди для куска который будет загружаться
 
-      setTimeout(function(){ $(this).trigger("loaded") }.bind(this), 1000);
+      setTimeout(function(){ $(this).trigger("loaded") }.bind(this), 1000); // генерируем событие для загрузки спустя 1 сек
     }
   }
+
+
   if(this.playable){
     this.partCounter++;
   }
@@ -117,16 +122,14 @@ Player.prototype.update = function() {
 }
 
 Player.prototype.pauseVideo = function(){
-  itemId = this.globalId+(this.counter-1);
-  item = document.getElementById(itemId);
+  item = document.getElementById(this.currentVideoId);
   item.pause();
   this.playable = false;
 }
 Player.prototype.playVideo = function(){
   this.playable = true;
   if(!this.newTime){
-    itemId = this.globalId+(this.counter-1);
-    item = document.getElementById(itemId);
+    item = document.getElementById(this.currentVideoId);
     item.play();
   }
 }
@@ -135,7 +138,6 @@ Player.prototype.startVideo = function(){
   this.update();
   var self = this; // hack for 'this' forwarding into update
   setInterval(function(){self.update.call(self)}, 1000);
-
 }
 
 Player.prototype.setPath = function(time){
@@ -167,18 +169,10 @@ Player.prototype.goTo = function(value){
 
 Player.prototype.createDom = function(){
   $('#' + this.globalId).append('<div id="'+this.wrapperId+'"></div>')
-  $('#' + this.wrapperId).append('<div id="player_videos"></div>')
-  $('#' + this.wrapperId).append('<div class="controls"></div>')
-  $('#' + this.wrapperId + ' .controls').append('<div class="play-controls"></div>')
-  $('#' + this.wrapperId + ' .controls').append('<div class="rewind-controls"></div>')
-  $('#' + this.wrapperId + ' .controls .play-controls').append('<button class="player-controls play"></button>')
-  $('#' + this.wrapperId + ' .controls .play-controls').append('<button class="player-controls pause"></button>')
-  $('#' + this.wrapperId + ' .controls .play-controls').append('<button class="player-controls fullscreen"></button>')
-  $('#' + this.wrapperId + ' .controls .play-controls').append('<input class="player-controls volume" type="range" min="0" max="100" value="70" />')
-  $('#' + this.wrapperId + ' .controls .rewind-controls').append('<input class="player-controls rewind" type="range" min="0" max="'+this.parts+'" value="0" />')
+  $('#' + this.wrapperId).append('<div id="player_videos"></div><div class="controls"><div class="play-controls"><button class="player-controls play"></button><button class="player-controls pause"></button><button class="player-controls fullscreen"></button><input class="player-controls volume" type="range" min="0" max="100" value="70"></div><div class="rewind-controls"><input class="player-controls rewind" type="range" min="0" max="' + this.parts + '" value="0"></div></div>');
 
-  this.playerItem = document.getElementById('player_videos');
-  this.setEvents()
+  this.playerItem = document.getElementById('player_videos'); // переменная в которой хранится див в котором циклически меняются видео
+  this.setEvents() // выставляем реакцию на события
 }
 
 Player.prototype.setEvents = function(){
@@ -192,6 +186,8 @@ Player.prototype.setEvents = function(){
 
   $('.player-controls.rewind').on('change', { player: this }, function(event){
     event.data.player.goTo($(this).val());
+    // event.data.player is Player
+    // $(this) is $('.player-controls.rewind')
   })
 
   $('.player-controls.volume').on('change', { player: this }, function(event){
@@ -203,10 +199,10 @@ Player.prototype.setEvents = function(){
   })
 
   $('#player_videos').on('click', { player: this }, function(event){
-    if(event.data.player.playable){
-      event.data.player.pauseVideo();
+    if(event.data.player.playable){ // если играет
+      event.data.player.pauseVideo(); // останавливаем
     } else {
-      event.data.player.playVideo();
+      event.data.player.playVideo(); // иначе играем
     }
   })
 }
@@ -231,12 +227,12 @@ Player.prototype.loadEnded = function(){
 }
 
 Player.prototype.toggleFullScreen = function(){
+  var wrapper = $('#' + this.wrapperId);
   if(this.fullscreen){
     this.fullscreen = false;
     wrapper.attr("style", "");
   } else {
     this.fullscreen = true;
-    wrapper = $('#' + this.wrapperId);
     style = "position: fixed; right: 0; bottom: 0; min-width: 100%; min-height: 100%; width: auto; height: auto; z-index: 100;"
     wrapper.attr("style", style);
   }
